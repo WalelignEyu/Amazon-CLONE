13ba176f6f0806a80fc9baf5a6ecca1aa04d4c0f
Eyerusalem <eyerusalemwale13@gmail.com>
2024-05-28 01:44:29 -0400

    api changed

--- src/Api/Axios.js
+++ src/Api/Axios.js
@@ -3,12 +3,12 @@ import axios from 'axios'
 const axiosInstance = axios.create({
 	//local instance of firebase functions
 	// baseURL: "http://127.0.0.1:5001/clone-b033a/us-central1/api",
-	baseURL: "http://localhost:5001",
+	// baseURL: "http://localhost:5001",
 
 	// deployed version of firebase function
 	// baseURL: "https://api-rfuhblh3ka-uc.a.run.app/",
 	// deployed version of amazon server on render.com
-	// baseURL: "https://amazon-api-deploy-mn4t.onrender.com/",
+	baseURL: "https://amazon-api-deploy-mn4t.onrender.com/",
 });
 
 export { axiosInstance };
